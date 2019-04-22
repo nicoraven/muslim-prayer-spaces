cd36a3b5fe7c9515cd51d6e67b7a705116fcbf1c
khyreerusydi <nicoraventbh@gmail.com>
2019-04-22 11:27:44 +0800

    updated table structures

--- seed.sql
+++ seed.sql
@@ -1,31 +1,33 @@
-DROP TABLE mosque;
+DROP TABLE space;
 
-CREATE TABLE mosque(
-    id SERIAL PRIMARY KEY,
-    name TEXT,
-    address TEXT,
-    latitude FLOAT,
-    longitude FLOAT,
-    telephone TEXT,
-    email TEXT,
-    website TEXT,
-    type TEXT
+CREATE TABLE space(
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	type TEXT,
+	address TEXT,
+	latitude FLOAT,
+	longitude FLOAT,
+	telephone TEXT,
+	email TEXT,
+	website TEXT,
+	directions TEXT,
+	details TEXT,
+	createdBy TEXT,
+	modifiedBy TEXT
 );
 
-INSERT INTO mosque (name, address, latitude, longitude, telephone, email, website, type) VALUES ('Masjid Al-Falah', '22 Bideford Road #01-01 Singapore 229923', '1.303590', '103.836908', '62353172', 'alfalah@alfalah.mosque.org.sg', 'http://www.alfalah.org.sg', 'mosque');
-INSERT INTO mosque (name, address, latitude, longitude, telephone, email, website, type) VALUES ('Masjid Abdul Hamid Kampung Pasiran', '10 Gentle Road Singapore 309194', '1.320033', '103.841059', '62512729', 'info@mahkp.org.sg', 'https://www.mahkp.org.sg', 'mosque');
+INSERT INTO space (name, type, address, latitude, longitude, telephone, email, website, directions, details, createdBy, modifiedBy) VALUES ('Masjid Al-Falah', 'mosque', '22 Bideford Road #01-01 Singapore 229923', '1.303590', '103.836908', '62353172', 'alfalah@alfalah.mosque.org.sg', 'http://www.alfalah.org.sg', NULL, NULL, 'admin', NULL);
+INSERT INTO space (name, type, address, latitude, longitude, telephone, email, website, directions, details, createdBy, modifiedBy) VALUES ('Masjid Abdul Hamid Kampung Pasiran', 'mosque',  '10 Gentle Road Singapore 309194', '1.320033', '103.841059', '62512729', 'info@mahkp.org.sg', 'https://www.mahkp.org.sg', NULL, NULL, 'admin', NULL);
+INSERT INTO space (name, type, address, latitude, longitude, telephone, email, website, directions, details, createdBy, modifiedBy) VALUES ('Masjid Muhajirin', 'mosque', '275 Braddell Road Singapore 579704', '1.343227', '103.852183', '62561166', 'lpm@muhajirin.mosque.org.sg', 'https://www.facebook.com/muhajirinsg', NULL, NULL, 'admin', NULL);
+INSERT INTO space (name, type, address, latitude, longitude, telephone, email, website, directions, details, createdBy, modifiedBy) VALUES ('International Plaza', 'musollah', '10 Anson Rd, Singapore 079903', '1.2757052', '103.8436157', NULL, NULL, NULL, 'Level 7, carpark. Take carpark lift 19 and 20 near Watsons store. Upon exiting the lift lobby on level 7 and facing the carpark, turn right, there is a small corner on the left of Staircase G which is the musollah.', 'Sejadah, slippers and telekong are provided. No toilet on the same level, so either can use the toilet on level 1, 6 & 8 for ablution.', 'admin', NULL);
+INSERT INTO space (name, type, address, latitude, longitude, telephone, email, website, directions, details, createdBy, modifiedBy) VALUES ('Fairmont Singapore', 'musollah', '80 Bras Basah Rd, Singapore 189560', '1.2940991', '103.8515279', NULL, NULL, NULL, 'The prayer room is located on the left side of the corridor passage outside the Asian Market Cafe at Level 2 Fairmont Hotel.', 'Ablution at the toilet and prayer garment is provided. A separate prayer room for males is available across a narrow hall next to ladies.', 'admin', NULL);
+INSERT INTO space (name, type, address, latitude, longitude, telephone, email, website, directions, details, createdBy, modifiedBy) VALUES ('Centennial Tower', 'musollah', '3 Temasek Ave, Singapore 039190', '1.293309', '103.8581653', NULL, NULL, NULL, 'Take the lift to Level B1, and use the left Exit door. At the end of the carpark on the right towards Lot 27, you will see a surau available.', 'Muslimin can pray on the outside carpeted area, muslimah can pray in the room. Prayer mats and telekung are provided. Ablution can be done at the wash area 3 lots to the left of the musollah.', 'admin', NULL);
 
-DROP TABLE mussolah;
+DROP TABLE changelog;
 
--- CREATE TABLE mussolah(
---     id SERIAL PRIMARY KEY,
---     name TEXT,
---     address TEXT,
---     latitude FLOAT,
---     longitude FLOAT,
---     directions TEXT,
---     details TEXT,
---.....type TEXT
--- );
-
--- INSERT INTO mussolah (name, address, latitude, longitude, directions, details, type) VALUES ('City Square Mall', 'Kitchener Rd, S208539', 'mussolah');
+CREATE TABLE changelog(
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	createdBy TEXT,
+	modifiedBy TEXT
+);
